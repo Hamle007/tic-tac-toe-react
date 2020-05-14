6bbd506b7fd9d591c2b06cf69297b16d40dbe460
Hamle007 <941745735@qq.com>
2020-05-14 22:56:25 +0800

    棋盘显示优化

--- src/index.js
+++ src/index.js
@@ -25,24 +25,25 @@ import './index.css';
     }
   
     render() {
-  
+      const element = (
+        <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+      </div>
+      )
+      let list = []
+      for (var i = 0; i < 3; i++) {
+        list.push((<div className="board-row">
+          {element}
+        </div>))
+        
+      }
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {list.map((square) => {
+            return square
+          })}
         </div>
       );
     }
